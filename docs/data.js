a60ee7ecd803bed17db6c127b38cdbbd55960189
github-action-benchmark <github@users.noreply.github.com>
2025-07-06 23:33:28 +0000

    add Rust Benchmark (cargo) benchmark result for 8c89569d06886c62d5af520a25bcdc70862408fe

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1751844602924,
+  "lastUpdate": 1751844808466,
   "repoUrl": "https://github.com/skanehira/ghost",
   "entries": {
     "Rust Benchmark": [
@@ -779,6 +779,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.03,
             "range": "± 1.60",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8c89569d06886c62d5af520a25bcdc70862408fe",
+          "message": "chore(deps): bump notify from 8.0.0 to 8.1.0 (#4)\n\nBumps [notify](https://github.com/notify-rs/notify) from 8.0.0 to 8.1.0.\n- [Release notes](https://github.com/notify-rs/notify/releases)\n- [Changelog](https://github.com/notify-rs/notify/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/notify-rs/notify/compare/notify-8.0.0...notify-8.1.0)\n\n---\nupdated-dependencies:\n- dependency-name: notify\n  dependency-version: 8.1.0\n  dependency-type: direct:production\n  update-type: version-update:semver-minor\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-07-07T08:31:56+09:00",
+          "tree_id": "d97f79d70a8bc82a7ffe15d38ba8601341d46d79",
+          "url": "https://github.com/skanehira/ghost/commit/8c89569d06886c62d5af520a25bcdc70862408fe"
+        },
+        "date": 1751844807582,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.28",
             "unit": "ns/iter"
           }
         ]
