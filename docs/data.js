9e511d43588903de10158087bfce65a7280b5462
github-action-benchmark <github@users.noreply.github.com>
2025-08-23 02:01:30 +0000

    add Rust Benchmark (cargo) benchmark result for 0d6ddd74560d60ff6d509c91edecf32c4028d9ad

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755914449700,
+  "lastUpdate": 1755914490257,
   "repoUrl": "https://github.com/skanehira/ghost",
   "entries": {
     "Rust Benchmark": [
@@ -1409,6 +1409,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.02,
             "range": "± 0.29",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0d6ddd74560d60ff6d509c91edecf32c4028d9ad",
+          "message": "chore(deps): bump thiserror from 2.0.12 to 2.0.15 (#20)\n\nBumps [thiserror](https://github.com/dtolnay/thiserror) from 2.0.12 to 2.0.15.\n- [Release notes](https://github.com/dtolnay/thiserror/releases)\n- [Commits](https://github.com/dtolnay/thiserror/compare/2.0.12...2.0.15)\n\n---\nupdated-dependencies:\n- dependency-name: thiserror\n  dependency-version: 2.0.15\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-08-23T10:59:49+09:00",
+          "tree_id": "df4ab40ebfab8cb7247a412cfbf35fc12173b0b9",
+          "url": "https://github.com/skanehira/ghost/commit/0d6ddd74560d60ff6d509c91edecf32c4028d9ad"
+        },
+        "date": 1755914489778,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.09",
             "unit": "ns/iter"
           }
         ]
